c0f5f5233bf0e157b1afb510978a65878bb7756c
Feanor Max <2499626251@qq.com>
2023-07-23 21:08:07 +0800

    [update] xiaohongshu

---
+++ xiaohong.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void maxSubArraySum(vector<int> a, int size, int x) {
+  int max_so_far = a[0];
+  int curr_max = a[0];
+  int start = 0, end = 0, s = 0;
+
+  for (int i = 1; i < size; i++) {
+    curr_max = max(a[i], curr_max + a[i]);
+    if (max_so_far < curr_max) {
+      max_so_far = curr_max;
+      start = s;
+      end = i;
+    }
+    if (curr_max < 0) {
+      curr_max = 0;
+      s = i + 1;
+    }
+  }
+
+  int min_num = a[start];
+  // cout << "sum: " << max_so_far << " {";
+  for (int i = start; i <= end; i++) {
+    min_num = min(a[i], min_num);
+    // cout << a[i] << ",";
+  }
+  // cout << "}"<< endl;
+
+  if (start > 0 || end < size - 1) {
+    cout << max_so_far + x << endl;
+  } else {
+    cout << curr_max + max(0, min_num - x) << endl;
+  }
+}
+int main() {
+  vector<int> ex5{-5, -3};
+  maxSubArraySum(ex5, ex5.size(), -3);
+  vector<int> ex4{-5, -2, -1, -6, -3};
+  maxSubArraySum(ex4, ex4.size(), 0);
+  vector<int> ex3{5, -1, -5, -3, 2};
+  maxSubArraySum(ex3, ex3.size(), 0);
+  vector<int> ex2{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  maxSubArraySum(ex2, ex2.size(), 0);
+  vector<int> ex1{-2, -3, 4, -1, -2, 1, 5, -3};
+  maxSubArraySum(ex1, ex1.size(), 0);
+}
